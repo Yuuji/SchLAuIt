453d48972d4ef0ee3f259b5e3031aa85333b72fc
Yuuji <desu@yuuji.de>
2014-12-24 01:48:39 +0100

    implement modal popover

--- client/main/helper.js
+++ client/main/helper.js
@@ -6,3 +6,33 @@ Template.Layout.helpers({
 		return currentRoute && template === currentRoute.lookupTemplate() ? 'active' : '';
 	}
 });
+
+$.fn.my_popover = function(type) {
+	var el = $('#my_popover_shadow');
+	if (el.length===0) {
+		el = $('<div />').attr('id', 'my_popover_shadow').css({
+			position: 'fixed',
+			top: 0,
+			left: 0,
+			width: '100%',
+			height: '100%',
+			zIndex: 1000,
+			background: '#aaa',
+			opacity: '.6'
+		}).hide();
+
+		$('body').append(el);
+	}
+
+	switch(type) {
+		case 'hide':
+			el.hide();
+			break;
+		case 'show':
+		default:
+			el.show();
+			break;
+	}
+
+	$.fn.popover.apply(this, arguments);
+};
